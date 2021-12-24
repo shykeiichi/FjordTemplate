97ae6bc1e38e2beaa66e30eaff84f53601ef1c75
willmexe <imdark200@gmail.com>
2021-12-24 18:24:58 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Fjord;
+using Fjord.Modules.Game;
+
+namespace Template_Game {
+    // Scene class this is where your game is 
+
+    public class main : scene
+    {
+        public override void on_load()
+        {
+            // This is where you load all your scenes 
+            // The if statement is so that it doesn't trigger multiple times
+
+            if(!scene_handler.get_scene("game-template")) {
+
+                // Add all scenes
+                scene_handler.add_scene("game-template", new main());
+
+                // Load the first scene this can later be called in any file as for example a win condition to switch scene.
+                scene_handler.load_scene("game-template");
+            }
+        }
+
+        // Update method
+        // This is where all your gamelogic is
+
+        public override void update()
+        {
+
+        }
+
+        // Render method
+        // This is where all your rendering is
+
+        public override void render()
+        {
+
+        }
+    }
+
+    // Main Class
+
+    class Program 
+    {
+        public static void Main(string[] args) 
+        {
+            // Function that starts game
+            // The parameter should be your start scene
+            game.set_resource_folder("resources");
+            game.run(new main());
+        }
+    }
+}
